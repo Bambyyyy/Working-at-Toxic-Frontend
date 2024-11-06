632d2001b38e93464ffadb9af5509998b99b2124
Flood <flod96@hotmail.com>
2024-11-06 21:57:33 +0100

    Fetch series by ID in PDP page and added styling

--- codetest/src/app/series/[id]/page.tsx
+++ codetest/src/app/series/[id]/page.tsx
@@ -1,12 +1,51 @@
+import { RetrievedSeriesType } from "@/app/types/Series";
 import React from "react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-export default function PDP() {
+type Params = {
+  id: string;
+};
+
+export default async function PDP(props: { params: Promise<Params> }) {
+  const params = await props.params;
+  const id = params.id;
+  const apikey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/${id}?api_key=${apikey}&language=en-US`
+  );
+  const serieData: RetrievedSeriesType = await res.json();
+
+  if (!serieData) {
+    notFound();
+  }
+
   return (
-    <main>
-      <div className="flex w-full">
-        <div className="w-1/2">Bild här</div>
-        <div className="w-1/2">Info här</div>
-      </div>
+    <main className="max-w-[80rem] mx-auto my-10 px-5 xl:p-0">
+      {serieData ? (
+        <div className="flex flex-col md:flex-row w-full">
+          <div className="w-full lg:w-1/2 relative aspect-[10/11]">
+            <Image
+              src={
+                serieData.poster_path !== null
+                  ? `https://image.tmdb.org/t/p/w500${serieData.poster_path}`
+                  : "/posternotfound.png"
+              }
+              alt={serieData.name}
+              fill
+              className="object-cover"
+              sizes="50vw"
+            />
+          </div>
+          <div className="w-full lg:w-1/2 p-4">
+            <h2 className="text-3xl my-5">{serieData.name}</h2>
+            <p>{serieData.overview}</p>
+          </div>
+        </div>
+      ) : (
+        <div>Inte laddat ännu</div>
+      )}
     </main>
   );
 }
