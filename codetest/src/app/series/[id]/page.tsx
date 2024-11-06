b05a0ca8e0f579e8c086dcd14550dc471afc77a3
Flood <flod96@hotmail.com>
2024-11-06 21:30:45 +0100

    Created dynamic route for Product Detail Page

---
+++ codetest/src/app/series/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PDP() {
+  return (
+    <main>
+      <div className="flex w-full">
+        <div className="w-1/2">Bild här</div>
+        <div className="w-1/2">Info här</div>
+      </div>
+    </main>
+  );
+}
