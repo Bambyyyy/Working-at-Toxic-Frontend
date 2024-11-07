abb9712fe5e899a46cda90ddee8847d568f6c010
Flood <flod96@hotmail.com>
2024-11-07 23:56:53 +0100

    Add Link element around CodeTest in header

--- codetest/src/app/components/Header.tsx
+++ codetest/src/app/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Search from "./Search";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="w-full h-14 bg-black">
       <div className="max-w-[80rem] h-full mx-auto flex justify-between items-center px-4 text-white">
         <div>
-          <h1>CodeTest</h1>
+          <Link href={"/"}>
+            <h1>CodeTest</h1>
+          </Link>
         </div>
         <Search />
       </div>
