3d703d954de748ea746ba31cee5dc0ea9d04b868
Flood <flod96@hotmail.com>
2024-11-07 22:18:03 +0100

    Complete search functionality with responsive results

--- codetest/src/app/components/Search.tsx
+++ codetest/src/app/components/Search.tsx
@@ -1,10 +1,26 @@
 "use client";
-import React, { useState } from "react";
-import { RetrievedSeriesType } from "../types/Series";
+import React, { useEffect, useState } from "react";
+import { RetrievedDataFromAPI, RetrievedSeriesType } from "../types/Series";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Search() {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<RetrievedSeriesType[]>([]);
+
+  useEffect(() => {
+    const fetchSerieData = async (query: string) => {
+      try {
+        const res = await fetch(`api/searchSeries?query=${query}`);
+        const data = await res.json();
+        console.log(data);
+        setSearchResults(data.results);
+      } catch (error) {
+        console.error();
+      }
+    };
+    fetchSerieData(query);
+  }, [query]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTimeout(() => {
@@ -13,12 +29,47 @@ export default function Search() {
   };
 
   return (
-    <div>
+    <div className="relative">
       <input
         type="text"
         placeholder="Sök..."
-        className="px-3 py-1 outline-none text-black rounded-lg"
+        onChange={handleInputChange}
+        className="px-3 py-1 outline-none text-black rounded-lg w-72"
       />
+      {searchResults && searchResults.length ? (
+        <div className="absolute top-12 left-0 w-full z-10 bg-white text-black flex flex-col items-start h-96 overflow-y-auto">
+          {searchResults.map((item) => (
+            <Link
+              href={`/`}
+              key={item.id}
+              className="hover:bg-gray-300 flex gap-x-2 w-full p-2"
+            >
+              <div className="relative w-16 h-20 overflow-hidden">
+                <Image
+                  src={
+                    item.poster_path !== null
+                      ? `https://image.tmdb.org/t/p/w500${item.poster_path}`
+                      : "/posternotfound.png"
+                  }
+                  alt={item.name}
+                  fill
+                  className="object-cover"
+                  sizes="10vw"
+                />
+              </div>
+              <div className="flex flex-col w-3/4 justify-evenly">
+                <p className="text-sm truncate max-w-44">{item.name}</p>
+                <p className="text-xs">
+                  Rating: {item.vote_average.toFixed(1)}
+                </p>
+                <p className="text-xs">
+                  LG: {item.original_language.toUpperCase()}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
