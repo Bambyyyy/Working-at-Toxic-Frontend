930b02befac3ca017fac23edab0cfc2c0558e1f4
Flood <flod96@hotmail.com>
2024-11-08 00:45:07 +0100

    Changed the delay on search function in the Search component

--- codetest/src/app/components/Search.tsx
+++ codetest/src/app/components/Search.tsx
@@ -11,7 +11,7 @@ export default function Search() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedQuery(query);
-    }, 2000);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, [query]);
